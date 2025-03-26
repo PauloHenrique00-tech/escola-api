7062a1b23065491b174aecac9be096a7e41134d9
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-03-26 12:00:12 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from "express";
+
+const app = express();
+const porta = 3000;
+
+/* CONFIGURANDO ROTAS */
+app.get('/', (req, res) => {
+    res.send(`API utilizando Node.js, Express e MySQL`);
+});
+// raiz da API
+app.get('/alunos', (req, res) => {
+    res.send(`Exibindo todos os alunos`);
+});
+
+// Exibindo TODOS os alunos
+app.get('/alunos', (req, res) => {
+    res.send(`Exibindo todos os alunos`);
+});
+
+// Exibindo UM aluno
+app.get('/alunos/1', (req, res) => {
+    res.send(`Exibindo dados de UM aluno`);
+});
+
+// INSERINDO/CADASTRANDO/ADICIONANDO um aluno
+app.post('/alunos', (req, res) => {
+    res.send(`Exibindo dados de UM aluno`);
+});
+
+// ATUALIZANDO aluno
+app.patch('/alunos/1', (req, res) => {
+    res.send(`Atualizando dados do aluno`);
+});
+
+// EXCLUINDO aluno
+app.delete('/aluno/1', (req, res) => {
+    res.send(`Aluno excluído com sucesso!`);
+});
+
+app.listen(porta, (req, res) => {
+    console.log(`Servidor rodando na porta ${porta}`);    
+});
