6bc4b2a69f16eb7fcd1a6f639e0da309fd1d5df4
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-04-02 11:40:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { ler, inserir, lerUm, excluir } from "./src/aluno.js";
+import { ler, inserir, lerUm, excluir, atualizar } from "./src/aluno.js";
 
 const app = express();
 const porta = 3000;
@@ -49,9 +49,16 @@ app.post('/alunos', (req, res) => {
 });
 
 // // ATUALIZANDO aluno
-//app.patch('/alunos/:id', (req, res) => {
+app.patch('/alunos/:id', (req, res) => {
     //res.send(`Atualizando dados do aluno`);
-//});
+
+    // capturar id
+    const id = parseInt(req.params.id);
+    // pegando as informações do body
+    const aluno = req.body;
+
+    atualizar(id, aluno, res);
+});
 
 // EXCLUINDO aluno
 app.delete('/alunos/:id', (req, res) => {
